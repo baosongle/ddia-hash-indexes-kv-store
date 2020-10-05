aa30bb9e047e89911949c3175dcc896f55cbf8d6
baosongle <songlebao@outlook.com>
2020-10-05 16:54:34 +0800

    Initial commit

---
+++ src/main/java/baosongle/hashindexkvstore/HashIndexKvStoreApplication.java
@@ -0,0 +1,11 @@
+package baosongle.hashindexkvstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashIndexKvStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HashIndexKvStoreApplication.class, args);
+    }
+}
---
+++ src/test/java/baosongle/hashindexkvstore/HashIndexKvStoreApplicationTests.java
@@ -0,0 +1,11 @@
+package baosongle.hashindexkvstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashIndexKvStoreApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
